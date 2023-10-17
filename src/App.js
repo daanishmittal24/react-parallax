6f8252f9e38a5414bc7d6d989c34f946cf8acaeb
Daanish Mittal <daanishmittal24@gmail.com>
2023-10-18 00:31:25 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -6,33 +6,38 @@ function App() {
   return (
     <div className="App">
       <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
-        <ParallaxLayer offset={0} speed={0.25}>
+        { <ParallaxLayer offset={0} speed={0.2}>
           <div class="animation_layer parallax" id="artback"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.3}>
-          <div class="animation_layer parallax" id="mountain"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.1}>
-          <div class="animation_layer parallax" id="logoland"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.3}>
-          <div class="animation_layer parallax" id="jungle1"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.35}>
-          <div class="animation_layer parallax" id="jungle2"></div>
-        </ParallaxLayer>
+        </ParallaxLayer> }
+
+        { <ParallaxLayer  offset={0} speed={0.5}>
+        <div class="animation_layer parallax" id="manonmountain"></div>
+        </ParallaxLayer> }
+
         <ParallaxLayer offset={0} speed={0.5}>
-          <div class="animation_layer parallax" id="jungle3"></div>
+        <div class="animation_layer parallax" id="mountain"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.45}>
-          <div class="animation_layer parallax" id="jungle4"></div>
+
+        <ParallaxLayer offset={0} speed={0.1}>
+          <div class="animation_layer parallax" id="right2_baloon"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.40}>
-          <div class="animation_layer parallax" id="manonmountain"></div>
+
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div class="animation_layer parallax" id="logo"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.35}>
-          <div class="animation_layer parallax" id="jungle5"></div>
+
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="cen_baloon"></div>
         </ParallaxLayer>
+
+        <ParallaxLayer style={{float:"right"}} offset={0} speed={0.45}>
+          <div class="animation_layer parallax" id="right_baloon"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.25}>
+        <div class="animation_layer parallax" id="logoland"></div>
+        </ParallaxLayer>
+
         <ParallaxLayer offset={1} speed={0.25}>
           <TextBlock />
         </ParallaxLayer>
